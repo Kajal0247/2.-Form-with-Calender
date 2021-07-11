37423a246e5979c8948593bedef2f6286728e69d
Kajal0247 <kajallohia3347@gmail.com>
2021-07-11 21:43:15 +0530

    final commit

---
+++ Ass. V.T.1/in.js
@@ -0,0 +1,6 @@
+
+        function loginForm() {
+            document.myform.submit();
+            document.myform.action = "https://crunchify.com/wp-content/uploads/code/Crunchify-LoginPage.html";
+        }
+    
